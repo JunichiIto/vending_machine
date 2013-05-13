983e512171c08b72e7e9ca4174cae251f8e026bb
Junichi Ito <jit@sonicgarden.jp>
2013-05-14 07:22:16 +0900

    Implement step 0

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -2,10 +2,50 @@ require 'rspec'
 require './vending_machine'
 
 describe VendingMachine do
-  describe "#test" do
-    specify do
-      machine = VendingMachine.new
-      expect(machine.say).to eq "hello!"
+  let(:machine) { VendingMachine.new }
+  describe "step 1" do
+    describe "#insert_money" do
+      it 'inserts 10 yen' do
+        expect(machine.insert_money(10)).to eq 10
+      end
+      it 'inserts 50 yen' do
+        expect(machine.insert_money(50)).to eq 50
+      end
+      it 'inserts 100 yen' do
+        expect(machine.insert_money(100)).to eq 100
+      end
+      it 'inserts 500 yen' do
+        expect(machine.insert_money(500)).to eq 500
+      end
+      it 'inserts 1000 yen' do
+        expect(machine.insert_money(1000)).to eq 1000
+      end
+      it 'inserts more than once' do
+        expect(machine.insert_money(10)).to eq 10
+        expect(machine.insert_money(10)).to eq 10
+      end
+    end
+    describe '#total_amount' do
+      context 'more than once' do
+        before do
+          machine.insert_money 10
+          machine.insert_money 50
+        end
+        specify { expect(machine.total_amount).to eq 60 }
+      end
+    end
+    describe '#change' do
+      before do
+          machine.insert_money 10
+          machine.insert_money 50
+      end
+      it 'returns change' do
+        expect(machine.change).to eq 60
+      end
+      it 'has no money' do
+        machine.change
+        expect(machine.total_amount).to eq 0
+      end
     end
   end
 end
--- vending_machine.rb
+++ vending_machine.rb
@@ -1,5 +1,18 @@
 class VendingMachine
-  def say
-    "hello!"
+  attr_reader :total_amount
+
+  def initialize
+    @total_amount = 0
+  end
+
+  def insert_money amount
+    @total_amount += amount
+    amount
+  end
+
+  def change
+    change = @total_amount
+    @total_amount = 0
+    change
   end
 end
